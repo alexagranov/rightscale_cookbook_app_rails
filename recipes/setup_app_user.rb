75dd22f089c9aa1b925f3527c6ea664bc87f4241
Alex Agranov <alex@morphogenic.net>
2014-01-31 17:23:15 -0500

    oops, forgot to create group first

--- recipes/setup_app_user.rb
+++ recipes/setup_app_user.rb
@@ -7,6 +7,14 @@ rightscale_marker
 
 log "  Setup the user/group account to execute the Rails application."
 
+# create the group first
+group node[:app_rails][:group] do
+  action :create
+  system true
+  append true
+end
+
+# ...then create the user
 user node[:app_rails][:user] do
   gid node[:app_rails][:group]
   shell "/bin/bash"
